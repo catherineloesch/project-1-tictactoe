7bf467b382dea2f709a9068847654d99d37677d9
Catherine Loesch <catherine.loesch@protonmail.com>
2023-03-29 15:29:43 +0100

    wireframe + pseudocode added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("Ready to play a round of Tic Tac Toe")
+
+//when new game starts, make sure cells are all empty
+//determine current player
+//making a move
+    //get user input
+    //check if position is available on board
+        //if position is taken, tell user to pick other cell i.e. make new input
+        //if position is available, make the move
+        //display move on board
+//check if there is a winner or if board is full
+    //if there is a winner
+        //tell the user they won
+        //update score board
+    //if board is full
+        //tell users it's a draw
+    //if there is no winner and no draw
+        //select new current player
