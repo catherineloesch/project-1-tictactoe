e37fb6940f08d2c8cb5f39ba0556d8742fad3f03
Catherine Loesch <catherine.loesch@protonmail.com>
2023-03-30 09:43:35 +0100

    added winning combinations+object for each player to keep track of wins; if a player wins overlay appears on screen

--- js/main.js
+++ js/main.js
@@ -15,15 +15,41 @@
     //if there is no winner and no draw
         //select new current player
 
+const xMove = 'x'; // setting CSS 'x' class to a variable
+const oMove = 'o'; // setting CSS 'o' class to a variable
 
-let xCurrentSymbol;
-const xMove = 'x';
-const oMove = 'o';
+const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal combinations
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical combinations
+    [0, 4, 8], [2, 4, 6] //diagonal combinations
+]
+
+//default: player 1 has x and player 2 has o
+let player1 = {
+    name: "Player 1",
+    symbol: xMove,
+    wins: 0
+}
+
+let player2 = {
+    name: "Player 2",
+    symbol: oMove,
+    wins: 0
+}
 
 //Selecting all cells in the grid + save to a variable
 const allCells = document.querySelectorAll('.cell') // -> NodeList(9)
+
 //Selecting the whole grid + save to a variable
 const grid = document.querySelector('.grid')
+
+//message: overlay that appears only when game ends -> select and save to variable
+const message = document.querySelector('.message')
+
+//winner announcement: text that appears inside message if either player wins the game
+const winnerAnnouncement = document.querySelector('.winner-announcement')
+
+let xCurrentSymbol; // true if current symbol is x, false if current symbol is o
 
 initialiseGame()
 
@@ -51,11 +77,21 @@ function handleUserInput(e) {
     const currentSymbol = xCurrentSymbol ? xMove : oMove
     // display move on grid
     displayNewMove(cell, currentSymbol)
-    switchTurns()
-    activateHoverSymbol()
-        // check if there is a winner or if board is full
-        // if there is no winner and no draw
-        // select new current player
+
+    // check if current player won or if grid is full
+    if (checkForWin(currentSymbol)) {   //if one of the player won
+        gameEnd(false)                  //the game ends
+    } else if (checkForDraw()) {        //if there is a draw
+        endGame(true)                   //the game ends
+    } else {                            //if no win or draw the game continues
+        switchTurns()                   //and it's the other player's turn
+        activateHoverSymbol()
+    }
+    // check if current player won or if grid is full
+
+
+    // if there is no winner and no draw
+    // select new current player
 }
 
 function displayNewMove(cell, currentSymbol) {
@@ -77,4 +113,26 @@ function activateHoverSymbol() {
     } else {
         grid.classList.add(oMove)
     }
+}
+
+function checkForWin(currentSymbol) {
+    const result = winningCombinations.some((combo) => {
+        return combo.every(index => {
+            return allCells[index].classList.contains(currentSymbol)
+        })
+    })
+    return result
+
+}
+
+function checkForDraw() {}
+
+function gameEnd(draw) {
+    if (draw) {
+
+    } else {
+        winnerAnnouncement.innerText = `${xCurrentSymbol ? "X" : "O"} wins the Game!`
+    }
+    message.classList.add('display') //overlay appears only when class 'display' is added to message
+
 }
