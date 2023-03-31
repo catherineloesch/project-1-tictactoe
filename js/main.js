f5b53f748970ad11f5529629ee7780f9f0da870e
Catherine Loesch <catherine.loesch@protonmail.com>
2023-03-31 19:42:06 +0100

    CSS: grid diplay, screen divided into 3 sections

--- js/main.js
+++ js/main.js
@@ -20,10 +20,9 @@
 const xMove = 'x'; // setting CSS 'x' class to a variable
 const oMove = 'o'; // setting CSS 'o' class to a variable
 
-
 let xCurrentSymbol; // true if current symbol is x, false if current symbol is o
-let gamesPlayed = 0
-
+let gamesPlayed = 0;
+let drawScore = 0;
 const winningCombinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal combinations
     [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical combinations
@@ -46,36 +45,33 @@ let player2 = {
 //svg for sad face
 const sadFace = `<span class="draw-sad-face"> Draw! <svg class="icon sad-face" xmlns="http://www.w3.org/2000/svg" width="9rem" height="9rem" viewBox="0 0 20 20"><g transform="translate(20 0) scale(-1 1)"><path fill="currentColor" d="M7.5 9.5a1 1 0 1 0 0-2a1 1 0 0 0 0 2Zm6-1a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm.062 4.89a.5.5 0 0 1-.7-.075l-.003-.003a1.91 1.91 0 0 0-.137-.137a3.069 3.069 0 0 0-.507-.37c-.461-.27-1.187-.555-2.213-.555s-1.753.284-2.216.556a3.088 3.088 0 0 0-.508.37a1.92 1.92 0 0 0-.138.137l-.003.003a.5.5 0 0 1-.777-.63l.39.314l-.39-.313v-.001l.002-.001l.002-.002l.005-.006l.014-.018l.049-.054c.04-.043.098-.102.174-.17c.152-.138.375-.316.674-.491c.6-.353 1.5-.694 2.722-.694c1.221 0 2.12.34 2.72.694c.3.176.522.353.673.49a2.907 2.907 0 0 1 .222.226l.015.017l.005.006l.002.003s.001.002-.389.314l.39-.312a.5.5 0 0 1-.078.702ZM10 2a8 8 0 1 0 0 16a8 8 0 0 0 0-16Zm-7 8a7 7 0 1 1 14 0a7 7 0 0 1-14 0Z"/></g></svg></span>`
 
-
 //Selecting HTML elements + assigning them to variables
 const grid = document.querySelector('.grid')
 const allCells = document.querySelectorAll('.cell') // -> NodeList(9)
-const message = document.querySelector('.message') // overlay that appears only when game ends (win or draw)
-const winnerAnnouncement = document.querySelector('.winner-announcement') //text text that appears on overlay after game ends (win or draw)
-const startNewGameBtn = document.querySelector('.message .btn') //button appears in overlay + starts new game after a game ends (win or draw)
+const player1Form = document.querySelector('#player-1-input-form')
+const player2Form = document.querySelector('#player-2-input-form')
 const nameInputPlayer1 = document.querySelector('#player-1-name-input')
 const nameInputPlayer2 = document.querySelector('#player-2-name-input')
 const player1NameDisplays = document.querySelectorAll('.display-name-player-1')
 const player2NameDisplays = document.querySelectorAll('.display-name-player-2')
 const playerTurnDisplay = document.querySelector('.current-player')
+const drawsDisplay = document.querySelector('.score-board .draw-score-reset .draw-score')
+let scorePlayer1 = document.querySelector('#player-1-score')
+let scorePlayer2 = document.querySelector('#player-2-score')
+const startGameBtn = document.querySelector('.center .start-game-btn')
+const startNewGameBtn = document.querySelector('.message .btn') //button appears in overlay + starts new game after a game ends (win or draw)
 const addInfoButtons = document.querySelectorAll('.add-player-info.btn') //NodeList(2)
-const player1Form = document.querySelector('#player-1-input-form')
-const player2Form = document.querySelector('#player-2-input-form')
 const btnsPlayerSubmitInfo = document.querySelectorAll('.btn-submit-player-input') //NodeList(2)
 const resetGameBtn = document.querySelector('.settings .btn-reset')
 const clearScoresBtn = document.querySelector('.btn.btn-clear-scores')
-let scorePlayer1 = document.querySelector('#player-1-score')
-let scorePlayer2 = document.querySelector('#player-2-score')
-
+const message = document.querySelector('.message') // overlay that appears only when game ends (win or draw)
+const winnerAnnouncement = document.querySelector('.winner-announcement') //text text that appears on overlay after game ends (win or draw)
 
 //Event Listeners
+startGameBtn.addEventListener('click', startGame)
 startNewGameBtn.addEventListener('click', initialiseGame)
 resetGameBtn.addEventListener('click', initialiseGame)
 clearScoresBtn.addEventListener('click', clearScores)
-
-allCells.forEach((cell) => { //attaching an eventListener to each cell so we know when player clicks on it
-    cell.addEventListener('click', handleUserInput,  {once: true}) //same cell can only be clicked once
-})
 
 addInfoButtons.forEach(btn => {
     btn.addEventListener('click', displayInputFields, {once: true})
@@ -86,14 +82,26 @@ btnsPlayerSubmitInfo.forEach(btn => {
 })
 
 //functions
+
+function startGame(){
+    startGameBtn.remove()
+    initialiseGame()
+}
+
 function initialiseGame() {
     //the starting symbol should switch at every turn
     if (gamesPlayed === 0) { //if this is the first game
         xCurrentSymbol = true;          // x symbol starts
+        playerTurnDisplay.innerText = `${player1.name}, it's your turn!`
+
     } else if (gamesPlayed%2 === 0) {//x symbol starts for even number or games
         xCurrentSymbol = true
+        playerTurnDisplay.innerText = `${player1.name}, it's your turn!`
+
     } else {
         xCurrentSymbol = false      //o symbol starts for odd number of games
+        playerTurnDisplay.innerText = `${player2.name}, it's your turn!`
+
     }
     
     allCells.forEach((cell) => {      //start with empty grid
@@ -126,8 +134,6 @@ function handleUserInput(e) {
         activateHoverSymbol()
     }
     // check if current player won or if grid is full
-
-
     // if there is no winner and no draw
     // select new current player
 }
@@ -183,6 +189,8 @@ function checkForDraw() { //returns true if every cell has either an x or an o
 
 function gameEnd(draw) {
     if (draw) {
+        drawScore++
+        drawsDisplay.innerHTML = `Draws: &nbsp;${drawScore}`
         winnerAnnouncement.innerHTML = `${sadFace} Neither player wins the game!`
     } else {
         if (xCurrentSymbol && (player1.symbol === xMove)
@@ -232,7 +240,7 @@ function handlePlayerInfoSubmit(e) {
         player2Form.classList.remove('display')
         console.log(`Player 2's name is ${player2.name}`)   
     }
-    
+
     addInfoButtons.forEach((btn) => {
         removeEventListener('click', displayInputFields, {once: true})
         btn.addEventListener('click', displayInputFields, {once: true})
@@ -244,7 +252,13 @@ function clearScores() {
     player2.wins = 0;
     scorePlayer1.innerHTML = 0;
     scorePlayer2.innerHTML = 0;
+    drawScore = 0;
+    drawsDisplay.innerHTML = `Draws: &nbsp;${drawScore}`
+    initialiseGame();
+
 }
 
-//when page loads, first game starts
-initialiseGame()
+//page loads
+grid.classList.remove(xMove)
+grid.classList.remove(oMove)
+allCells.forEach(cell => cell.removeEventListener('click', handleUserInput))
