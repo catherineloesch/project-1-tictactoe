81906dd6883a0a2b0a2774f2ff8d0efd8acb5e73
Catherine Loesch <catherine.loesch@protonmail.com>
2023-04-02 20:08:16 +0100

    trying to solve 404 for mp3 files

--- js/audio.js
+++ js/audio.js
@@ -2,37 +2,37 @@
 //Howler.js library used (https://howlerjs.com/ | https://cdnjs.com/libraries/howler)
 
 const soundWin = new Howl({
-    src: ['./../assets/audio/success.mp3', './../assets/audio/success.wav', './../assets/audio/success.webm'],
+    src: ['../audio/success.mp3', '../audio/success.wav', '../audio/success.webm'],
     volume: 0.5
 })
 
 const soundDraw = new Howl({
-    src: ['./../assets/audio/draw.mp3', './../assets/audio/draw.wav', './../assets/audio/draw.webm'],
+    src: ['../audio/draw.mp3', '../audio/draw.wav', '../audio/draw.webm'],
     volume: 0.4
 })
 
 const soundMakeMove = new Howl({
-    src: ['./../assets/audio/clicks.mp3', './../assets/audio/clicks.wav', './../assets/audio/clicks.webm'],
+    src: ['../audio/clicks.mp3', '../audio/clicks.wav', '../audio/clicks.webm'],
     volume: 0.5
 })
 
 const soundClick = new Howl({
-    src: ['../audio/mouseClick.mp3', './audio/mouseClick.wav', './audio/mouseClick.webm'],
+    src: ['../audio/mouseClick.mp3', '../audio/mouseClick.wav', '../audio/mouseClick.webm'],
     volume: 0.5
 })
 
 const soundResetScores = new Howl({
-    src: ['./../assets/audio/game_button.mp3', './../assets/audio/game_button.wav', './../assets/audio/game_button.webm'],
+    src: ['../audio/game_button.mp3', '../audio/game_button.wav', '../audio/game_button.webm'],
     volume: 0.5
 })
 
 const soundResetGame = new Howl({
-    src: ['./../assets/audio/processing.mp3', './../assets/audio/processing.wav', './../assets/audio/processing.webm'],
+    src: ['../audio/processing.mp3', '../audio/processing.wav', '../audio/processing.webm'],
     volume: 1
 })
 
 const soundOn = new Howl({
-    src: ['./../assets/audio/notification.mp3', './../assets/audio/notification.wav', './../assets/audio/notification.webm'],
+    src: ['../audio/notification.mp3', '../audio/notification.wav', '../audio/notification.webm'],
     volume: 0.5
 })
 
